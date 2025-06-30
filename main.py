d88af4bbf54f436c5b3a9d7aa347bc3b152529c8
K_Kamil <kkamil.kula@gmail.com>
2025-06-30 23:18:25 +0200

    refactor: reorganize project structure

--- main.py
+++ main.py
@@ -1,8 +1,2 @@
-import pandas as pd
-
-from src.DataManager import DataManager
-
 if __name__ == "__main__":
-    datasets = DataManager()
-    print(datasets._dataset)
-    print(datasets._user_data)
+    pass
